205485176f22f0d354566778e8a71635587d570c
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-07-04 10:07:47 -0300

    tentando exibir uma equipe

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
 import React, { Component } from "react";
 import Rout from "./routes";
-import api from './api';
+//import api from './api';
 
 class App extends Component{
-
-    state = {
-        equipes: [],
-    }
-    async componentDidMount(){
-        const response = await api.get('https://golang-posgre-brisanet.herokuapp.com/equipes/');
-        console.log(response.data);
-        
-        this.setState({equipes: response.data});
-    }
-
     render(){
         return(
             <div>
---
+++ src/components/CardEquipe/AllEquipes.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+//import Rout from "./routes";
+import api from '../../api';
+
+class AllEquipes extends Component{
+
+    state = {
+        equipes: [],
+    }
+    async componentDidMount(){
+        const response = await api.get('/equipes/');
+        
+        this.setState({equipes: response.data});
+    }
+
+    render(){
+        const { equipes } = this.state;
+
+        return(
+            <div>
+                {console.log(equipes)};
+                {equipes.map(equipes =>(
+                    <li key = {equipes.nome_equipe}>
+                        {console.log(equipes)}
+                    </li>
+                ))};
+            </div>
+        );
+    };
+};
+
+export default AllEquipes
