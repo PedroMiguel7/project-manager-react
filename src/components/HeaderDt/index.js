f5515d72ad634cfffede8077cdd03f1a0945fb4b
dayannebugarim <81201377+dayannebugarim@users.noreply.github.com>
2022-07-04 11:52:40 -0300

    Update index.js

--- src/components/HeaderDt/index.js
+++ src/components/HeaderDt/index.js
@@ -16,7 +16,7 @@ function HeaderDt({pagina, titulo, status}) {
             <div className='row mt-5 pb-3 main-header'>
                 <div className="d-flex align-items-center col-lg-4">
                     <h1 className="TituloDt fs-2">{pagina} <span className="nomeProjeto">{titulo}</span></h1>
-                    <div className="Status ms-4 mb-2 px-2">{status}</div>
+                    <div className="Status ms-4 mb-2 px-2" onClick={mudarStatus}>{status}</div>
                 </div>
                 <input className="col-lg-3 offset-lg-5" type="search" name="main-search" id="main-search" placeholder="Search here..."/>
             </div>
