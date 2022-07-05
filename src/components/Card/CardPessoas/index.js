f4f0a19ccb49ae9a1ae13c739121875e88989aa7
dayannebugarim <81201377+dayannebugarim@users.noreply.github.com>
2022-07-05 09:26:49 -0300

    arrumando os cards bugados

---
+++ src/components/Card/CardPessoas/index.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import aim from '../../../assets/icons/aim.svg'
+import profile from '../../../assets/icons/Profile.svg'
+//import PropTypes from 'prop-types';
+import api from '../../../api';
+
+class Cards extends Component {
+    state = {
+        projetos: [],
+    }
+    async componentDidMount() {
+        const response = await api.get('/projetos/');
+
+        console.log(response.data);
+
+        this.setState({ projetos: response.data });
+    }
+
+    render() {
+        const { projetos } = this.state;
+
+        return (
+            <div>
+                {projetos.map(p => (
+                    <Link to={'/projetos/'+ p.id_projeto} className="Link text-reset text-decoration-none">
+
+                        <div className="col-lg-3 col-md-12 Card p-4">
+                            <div key={projetos.id_projeto}>
+                                <div className="card-part1 mb-3">
+                                    <h2 class="fs-4">{p.nome_projeto}</h2>
+                                    <p className="description overflow-hidden">{p.descricao_projeto}</p>
+                                    <div className="mb-3">
+                                        <img src={aim} alt="" /> <span>{p.id_equipe}</span>
+                                    </div>
+                                </div>
+                                <div className="card-part2 d-flex justify-content-between">
+                                    <div className="card-members">
+                                        <h6>Membros</h6>
+                                        <div className='d-flex gap-1'>
+                                            <img src={profile} alt="" />
+                                            <img src={profile} alt="" />
+                                        </div>
+                                    </div>
+                                    <div className="card-progress">
+                                        <h6>Status</h6>
+                                        {p.status}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </Link>
+                ))}
+            </div>
+        )
+    }
+}
+
+
+export default Cards;
--- src/components/Card/CardProjetos/ExibirProjetos.js
+++ src/components/Card/CardProjetos/ExibirProjetos.js
@@ -21,37 +21,36 @@ class Cards extends Component {
         const { projetos } = this.state;
 
         return (
-            <div>
+            <>
                 {projetos.map(p => (
-                    <Link to={'/projetos/'+ p.id_projeto} className="Link text-reset text-decoration-none">
+                    <Link to={'/projetos/' + p.id_projeto} className="Link text-reset text-decoration-none col-lg-3 col-md-12 Card p-4">
 
-                        <div className="col-lg-3 col-md-12 Card p-4">
-                            <div key={projetos.id_projeto}>
-                                <div className="card-part1 mb-3">
-                                    <h2 class="fs-4">{p.nome_projeto}</h2>
-                                    <p className="description overflow-hidden">{p.descricao_projeto}</p>
-                                    <div className="mb-3">
-                                        <img src={aim} alt="" /> <span>{p.id_equipe}</span>
+                        <div key={projetos.id_projeto}>
+                            <div className="card-part1 mb-3">
+                                <h2 class="fs-4">{p.nome_projeto}</h2>
+                                <p className="description overflow-hidden">{p.descricao_projeto}</p>
+                                <div className="mb-3">
+                                    <img src={aim} alt="" /> <span>{p.id_equipe}</span>
+                                </div>
+                            </div>
+                            <div className="card-part2 d-flex justify-content-between">
+                                <div className="card-members">
+                                    <h6>Membros</h6>
+                                    <div className='d-flex gap-1'>
+                                        <img src={profile} alt="" />
+                                        <img src={profile} alt="" />
                                     </div>
                                 </div>
-                                <div className="card-part2 d-flex justify-content-between">
-                                    <div className="card-members">
-                                        <h6>Membros</h6>
-                                        <div className='d-flex gap-1'>
-                                            <img src={profile} alt="" />
-                                            <img src={profile} alt="" />
-                                        </div>
-                                    </div>
-                                    <div className="card-progress">
-                                        <h6>Status</h6>
-                                        {p.status}
-                                    </div>
+                                <div className="card-progress">
+                                    <h6>Status</h6>
+                                    {p.status}
                                 </div>
                             </div>
                         </div>
+
                     </Link>
                 ))}
-            </div>
+            </>
         )
     }
 }
