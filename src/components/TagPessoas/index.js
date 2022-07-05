cd6deebc09f2c2fa46fbef8f6a66998482922957
Dayanne <81201377+dayannebugarim@users.noreply.github.com>
2022-07-05 16:54:18 -0300

    tristeza e depressao

--- src/components/TagPessoas/index.js
+++ src/components/TagPessoas/index.js
@@ -1,34 +1,52 @@
 import {useState, useEffect} from 'react';
 
 function TagPessoas({funcao}) {
-    /*const [funcaoBackground, mudarBackground] = useState(0);
+    const [funcaoBackground, mudarBackground] = useState(0);
     
     useEffect(() => {
         const funcaoBackground = window.document.getElementsByClassName('CardPessoaTag');
 
-        if (funcao === "Front-End" || "Front End") {
-            funcaoBackground[0].style.background = '#FFC16A';
+        /*
+        switch (funcao) {
+            case ("Front-End" || "Front End"):
+                funcaoBackground[0].style.background = '#FFC16A';
+                break;
+            case ("Back-End" || "Back End"):
+                funcaoBackground[0].style.background = '#A9DFD8';
+                break;
+            case ("Dev-Ops" || "Dev Ops"):
+                funcaoBackground[0].style.background = '#F2C8ED';
+                break;
+            case ("Tester"):
+                funcaoBackground[0].style.background = '#A7CAFF';
+            
+        }*/
+    
+        /*if (funcao === "Front-End" || "Front End") {
+            funcaoBackground[4].style.background = '#FFC16A';
         } 
         if (funcao === "Back-End" || "Back End") {
-            funcaoBackground[0].style.background = '#A9DFD8';
+            funcaoBackground[1].style.background = '#A9DFD8';
         } 
         if (funcao === "Dev-Ops" || "Dev Ops") {
-            funcaoBackground[0].style.background = '#F2C8ED'
+            funcaoBackground[0].style.background = '#F2C8ED';
         } 
         if (funcao === "Tester") {
-            funcaoBackground[0].style.background = '#A7CAFF'
+            funcaoBackground[0].style.background = '#A7CAFF';
         } 
         if (funcao === "Mobile") {
-            funcaoBackground[0].style.background = '#E7DF9B'
+            funcaoBackground[0].style.background = '#E7DF9B';
         } 
         if (funcao === "Banco de Dados") {
-            funcaoBackground[0].style.background = '#F2A7A7'
-        }
-    })*/
+            funcaoBackground[0].style.background = '#F2A7A7';
+        }*/
+    })
 
     return (
         <>
-            <div className="CardPessoaTag" /*onLoad={() => mudarBackground(funcaoBackground)}*/>{funcao}</div>
+            <div className="CardPessoaTag" onLoad={() => mudarBackground(funcaoBackground)}>
+             {funcao}
+            </div>
         </>
     )
 }
