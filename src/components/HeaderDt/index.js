37d927d082472b455fac6b2587e8e2e1a43a2daa
dayannebugarim <81201377+dayannebugarim@users.noreply.github.com>
2022-07-07 11:27:36 -0300

    Update index.js

--- src/components/HeaderDt/index.js
+++ src/components/HeaderDt/index.js
@@ -5,24 +5,38 @@ function HeaderDt({pagina, titulo, status}) {
     const [statusBg, mudarStatus] = useState();
     console.log(statusBg);
 
-    useEffect(() => {
+    function setTag(sts) {
+        switch(sts) {
+            case "Em andamento" || "Em Andamento":
+                return '#28aef3b3';
+                break;
+            case "Concluído" || "Concluido":
+                return '#28f33cb3';
+                break;
+            case "Cancelado":
+                return '#898989b3';
+                break;
+        }
+    }
+
+    /*useEffect(() => {
         const statusBg = window.document.getElementsByClassName('Status');
 
         if (status === "Em andamento" || "Em Andamento") {
             statusBg[0].style.background = '#28aef3b3';
-        } else if (status === "Concluído" || "Concluido") {
+        } if (status === "Concluído" || "Concluido") {
             statusBg[0].style.background = '#28f33cb3';
-        } else if (status === "Cancelado") {
+        } if (status === "Cancelado") {
             statusBg[0].style.background = '#898989b3'
         }
-    })
+    })*/
 
     return (
         <>
             <div className='row mt-5 pb-3 main-header'>
                 <div className="d-flex align-items-center col-lg-4">
                     <h1 className="TituloDt fs-2">{pagina} <span className="nomeProjeto">{titulo}</span></h1>
-                    <div className="Status ms-4 mb-2 px-2" onChange={() => mudarStatus(statusBg)}>{status}</div>
+                    <div className="Status ms-4 mb-2 px-2" style={{backgroundColor:setTag(status)}}>{status}</div>
                 </div>
                 <input className="col-lg-3 offset-lg-5" type="search" name="main-search" id="main-search" placeholder="Search here..."/>
             </div>
