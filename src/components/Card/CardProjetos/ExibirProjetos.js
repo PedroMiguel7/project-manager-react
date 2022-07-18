fc84366619d59fbe7808117632ad49558c8676f3
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-07-18 10:23:11 -0300

    exibir projetos nos cards agora não quebra também se estiver nulo

--- src/components/Card/CardProjetos/ExibirProjetos.js
+++ src/components/Card/CardProjetos/ExibirProjetos.js
@@ -20,45 +20,85 @@ class Cards extends Component {
 
     }
 
+    exibeprojeto = (props) => {
+        var ProjetoS = 0;
+        if(props.projetos === null){
+            <div className="Link text-reset text-decoration-none col-lg-3 col-md-12 Card p-4">
+                    <div>
+                        <div className="card-part1 mb-3">
+                            <div className="d-flex justify-content-between">
+                                <h2 class="fs-4">Cadastre um projeto</h2>
+                                <Link to="/projetos">
+                                </Link>
+                            </div>
+                            <p className="description overflow-hidden">não tem descrição se não tem projeto</p>
+                            <div className="mb-3">
+                                <img src={aim} alt="" />
+                                <span>
+                                    sem equipe também, por motivos já ditos.
+                                </span>
+                            </div>
+                        </div>
+                        <div className="card-part2 d-flex justify-content-between">
+                            <div className="card-members">
+                                <h6>Membros</h6>
+                                <div className='d-flex gap-1'>
+                                    <img src={profile} alt="" />
+                                    <img src={profile} alt="" />
+                                </div>
+                            </div>
+                            <div className="card-progress">
+                                <h6>Status</h6>
+                                inexistente
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+        } else{
+            props.projetos.map(p => (
+                <Link reloadDocument to={'/projetos/' + p.id_projeto} className="Link text-reset text-decoration-none col-lg-3 col-md-12 Card p-4">
+                    <div key={props.projetos.id_projeto}>
+                        <div className="card-part1 mb-3">
+                            <div className="d-flex justify-content-between">
+                                <h2 class="fs-4">{p.nome_projeto}</h2>
+                                <Link to="/projetos">
+                                    <CardDelete id = {p.id_projeto}/>
+                                </Link>
+                            </div>
+                            <p className="description overflow-hidden">{p.descricao_projeto}</p>
+                            <div className="mb-3">
+                                <img src={aim} alt="" />
+                                <span>
+                                    {p.nome_equipe}
+                                </span>
+                            </div>
+                        </div>
+                        <div className="card-part2 d-flex justify-content-between">
+                            <div className="card-members">
+                                <h6>Membros</h6>
+                                <div className='d-flex gap-1'>
+                                    <img src={profile} alt="" />
+                                    <img src={profile} alt="" />
+                                </div>
+                            </div>
+                            <div className="card-progress">
+                                <h6>Status</h6>
+                                {p.status}
+                            </div>
+                        </div>
+                    </div>
+
+                </Link>
+            ))
+        }
+    }
+
     render() {
         const { projetos } = this.state;
         return (
             <>
-                {projetos.map(p => (
-                    <Link reloadDocument to={'/projetos/' + p.id_projeto} className="Link text-reset text-decoration-none col-lg-3 col-md-12 Card p-4">
-                        <div key={projetos.id_projeto}>
-                            <div className="card-part1 mb-3">
-                                <div className="d-flex justify-content-between">
-                                    <h2 class="fs-4">{p.nome_projeto}</h2>
-                                    <Link to="/projetos">
-                                        <CardDelete id = {p.id_projeto}/>
-                                    </Link>
-                                </div>
-                                <p className="description overflow-hidden">{p.descricao_projeto}</p>
-                                <div className="mb-3">
-                                    <img src={aim} alt="" />
-                                    <span>
-                                        {p.nome_equipe}
-                                    </span>
-                                </div>
-                            </div>
-                            <div className="card-part2 d-flex justify-content-between">
-                                <div className="card-members">
-                                    <h6>Membros</h6>
-                                    <div className='d-flex gap-1'>
-                                        <img src={profile} alt="" />
-                                        <img src={profile} alt="" />
-                                    </div>
-                                </div>
-                                <div className="card-progress">
-                                    <h6>Status</h6>
-                                    {p.status}
-                                </div>
-                            </div>
-                        </div>
-
-                    </Link>
-                ))}
+                <this.exibeprojeto projetos = {projetos}/>
             </>
         )
     }
