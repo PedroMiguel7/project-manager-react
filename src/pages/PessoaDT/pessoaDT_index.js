7a6c04064dc13bf50f0b13a82b67236dbd3f6746
dayannebugarim <81201377+dayannebugarim@users.noreply.github.com>
2022-07-19 10:41:00 -0300

    Update pessoaDT_index.js

--- src/pages/PessoaDT/pessoaDT_index.js
+++ src/pages/PessoaDT/pessoaDT_index.js
@@ -68,10 +68,10 @@ class PessoasDT extends Component {
 
                     </div>
                     <div className="col-lg-8">
-                        <div className="row Teste h-lg-50 p-4">
+                        <div className="row Teste h-lg-50 p-3">
                             Rendimento
                         </div>
-                        <div className="Teste row  h-lg-50 p-4">
+                        <div className="Teste row  h-lg-50 p-3">
                             
                             <HeaderTarefas />
                             
