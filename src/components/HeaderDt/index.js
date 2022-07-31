d6c8ffd07236176836b0e228bffbe7b71f3231ce
PedroMiguel7 <93842972+PedroMiguel7@users.noreply.github.com>
2022-07-31 18:53:11 -0300

    tirando barra de pesquisa de equipeDT

--- src/components/HeaderDt/index.js
+++ src/components/HeaderDt/index.js
@@ -38,7 +38,7 @@ function HeaderDt({pagina, titulo, status}) {
                     <h1 className="TituloDt fs-2">{pagina} <span className="nomeProjeto">{titulo}</span></h1>
                     <div className="Status ms-4 mb-2 px-2" style={{backgroundColor:setTag(status)}}>{status}</div>
                 </div>
-                <input className="col-lg-3 offset-lg-5" type="search" name="main-search" id="main-search" placeholder="Search here..."/>
+                
             </div>
         </>
     
