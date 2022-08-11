e76bd7af2da80d6002193ee80718b6e3e0603c55
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-08-11 08:25:20 -0300

    fazendo cards separados para home

--- src/components/Card/CardProjetos/ExibirProjetos.js
+++ src/components/Card/CardProjetos/ExibirProjetos.js
@@ -37,6 +37,47 @@ export default function Cards (props) {
           </div>
         </div>
       );
+    } else if(props.home === "home"){
+      props.Projetos.reverse();
+      return (
+        props.Projetos.map(p => (
+          <Link key={p.id_projeto}
+            reloadDocument
+            to={"/projetos/" + p.id_projeto}
+            className="Link text-reset text-decoration-none col-lg-3 col-md-12 Cardh p-3"
+          >
+            <div >
+              <div className="card-part1 mb-3">
+                <div className="d-flex justify-content-between">
+                  <h2 className="fs-4">{p.nome_projeto}</h2>
+                  <Link to="/projetos">
+                   <CardDelete id_projeto={p.id_projeto} atualiza={props.atualiza}/>
+                  </Link>
+                </div>
+                <p className="description overflow-hidden">
+                  {p.descricao_projeto}
+                </p>
+                <div className="mb-3">
+                  <img src={aim} alt="" />
+                  <span>{p.nome_equipe}</span>
+                </div>
+              </div>
+              <div className="card-part2 d-flex justify-content-between">
+                <div className="card-members">
+                  <h6>Progresso</h6>
+                  <div className="d-flex gap-1">
+                    {<BarraProgresso id_projeto={p.id_projeto} op={1} />}%
+                  </div>
+                </div>
+                <div className="card-progress">
+                  <h6>Status</h6>
+                  {p.status}
+                </div>
+              </div>
+            </div>
+          </Link>
+        ))
+      )
     } else {
       props.Projetos.reverse();
       
