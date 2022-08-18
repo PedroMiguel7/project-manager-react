21cf14842c703d4155353bdcebc27016c94d4a66
dayannebugarim <81201377+dayannebugarim@users.noreply.github.com>
2022-08-18 08:53:49 -0300

    add styled components

---
+++ src/components/Card/CardEquipe/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import TeamNotFound from "../../../assets/empty-states/team-not-found.svg";
+import EditaEquipe from "../CardEquipe/EdiçãoEquipe/EditaEquipe";
+import Avatar from '@mui/material/Avatar';
+import AvatarGroup from '@mui/material/AvatarGroup';
+
+export default function CardEquipe() {
+
+}
