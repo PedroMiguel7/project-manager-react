d47a4101948a6962b2f2332bac0219346aa117e0
dayannebugarim <81201377+dayannebugarim@users.noreply.github.com>
2022-08-18 11:49:00 -0300

    Update index.js

--- src/components/Card/CardEquipe/index.js
+++ src/components/Card/CardEquipe/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
-import api from "../../../api";
 import TeamNotFound from "../../../assets/empty-states/team-not-found.svg";
 import EditaEquipe from "../CardEquipe/EdiçãoEquipe/EditaEquipe";
 import Avatar from '@mui/material/Avatar';
