c35bba61e98d0aa5a5268701f69d6d1d85ede772
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-08-18 11:54:55 -0300

    Gráfico introduzido, mas ainda não puxando os dados

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -4,7 +4,7 @@ import Cards from "../../components/Card/CardProjetos/ExibirProjetos";
 import { useState, useEffect } from "react";
 import api from "../../api";
 import CardBaixoHome from "../Home/TabelaProjetosHome";
-import { ResponsiveMarimekko } from '@nivo/marimekko'
+import { ResponsiveMarimekko } from '@nivo/marimekko';
 
 export default function Menu_index() {
   const [projetos, setProjetos] = useState([]);
@@ -84,7 +84,7 @@ export default function Menu_index() {
     }
   }
 
-  /*function Grafico() {
+  function Grafico () {
     const data = [
       {
         "statement": "it's good",
@@ -128,206 +128,206 @@ export default function Menu_index() {
       }
     ]
 
-      < ResponsiveMarimekko
-    data = { data }
-    id = "statement"
-    value = "participation"
-    dimensions = {
-      [
-      {
-        id: 'disagree strongly',
-        value: 'stronglyDisagree'
-      },
-      {
-        id: 'disagree',
-        value: 'disagree'
-      },
-      {
-        id: 'agree',
-        value: 'agree'
-      },
-      {
-        id: 'agree strongly',
-        value: 'stronglyAgree'
-      }
+    const MyResponsivemike = () => (
+      <ResponsiveMarimekko
+      data={data}
+      id="statement"
+      value="participation"
+      dimensions={[
+          {
+              id: 'disagree strongly',
+              value: 'stronglyDisagree'
+          },
+          {
+              id: 'disagree',
+              value: 'disagree'
+          },
+          {
+              id: 'agree',
+              value: 'agree'
+          },
+          {
+              id: 'agree strongly',
+              value: 'stronglyAgree'
+          }
       ]}
-    innerPadding = { 9}
-    axisTop = { null}
-    axisRight = {{
-      orient: 'right',
-        tickSize: 5,
+      innerPadding={9}
+      axisTop={null}
+      axisRight={{
+          orient: 'right',
+          tickSize: 5,
           tickPadding: 5,
-            tickRotation: 0,
-              legend: '',
-                legendOffset: 0
-    }
-  }
-  axisBottom = {{
-    orient: 'bottom',
-      tickSize: 5,
-        tickPadding: 5,
           tickRotation: 0,
-            legend: 'participation',
-              legendOffset: 36,
-                legendPosition: 'middle'
-  }
-}
-axisLeft = {{
-  orient: 'left',
-    tickSize: 5,
-      tickPadding: 5,
-        tickRotation: 0,
+          legend: '',
+          legendOffset: 0
+      }}
+      axisBottom={{
+          orient: 'bottom',
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: 'participation',
+          legendOffset: 36,
+          legendPosition: 'middle'
+      }}
+      axisLeft={{
+          orient: 'left',
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
           legend: 'opinions',
-            legendOffset: -40,
-              legendPosition: 'middle'
-}}
-margin = {{ top: 40, right: 80, bottom: 100, left: 80 }}
-colors = {{ scheme: 'spectral' }}
-borderWidth = { 1}
-borderColor = {{
-  from: 'color',
-    modifiers: [
-      [
-        'darker',
-        0.2
-      ]
-    ]
-}}
-defs = {
-  [
-  {
-    id: 'lines',
-    type: 'patternLines',
-    background: 'rgba(0, 0, 0, 0)',
-    color: 'inherit',
-    rotation: -45,
-    lineWidth: 4,
-    spacing: 8
-  }
-  ]}
-fill = {
-  [
-  {
-    match: {
-      id: 'agree strongly'
-    },
-    id: 'lines'
-  },
-  {
-    match: {
-      id: 'disagree strongly'
-    },
-    id: 'lines'
-  }
-  ]}
-legends = {
-  [
-  {
-    anchor: 'bottom',
-    direction: 'row',
-    justify: false,
-    translateX: 0,
-    translateY: 80,
-    itemsSpacing: 0,
-    itemWidth: 140,
-    itemHeight: 18,
-    itemTextColor: '#999',
-    itemDirection: 'right-to-left',
-    itemOpacity: 1,
-    symbolSize: 18,
-    symbolShape: 'square',
-    effects: [
-      {
-        on: 'hover',
-        style: {
-          itemTextColor: '#000'
-        }
-      }
-    ]
-  }
-  ]}
+          legendOffset: -40,
+          legendPosition: 'middle'
+      }}
+      margin={{ top: 20, right: 40, bottom: 50, left: 40 }}
+      colors={{ scheme: 'spectral' }}
+      borderWidth={1}
+      borderColor={{
+          from: 'color',
+          modifiers: [
+              [
+                  'darker',
+                  0.2
+              ]
+          ]
+      }}
+      defs={[
+          {
+              id: 'lines',
+              type: 'patternLines',
+              background: 'rgba(0, 0, 0, 0)',
+              color: 'inherit',
+              rotation: -45,
+              lineWidth: 4,
+              spacing: 8
+          }
+      ]}
+      fill={[
+          {
+              match: {
+                  id: 'agree strongly'
+              },
+              id: 'lines'
+          },
+          {
+              match: {
+                  id: 'disagree strongly'
+              },
+              id: 'lines'
+          }
+      ]}
+      legends={[
+          {
+              anchor: 'bottom',
+              direction: 'row',
+              justify: false,
+              translateX: 0,
+              translateY: 80,
+              itemsSpacing: 0,
+              itemWidth: 140,
+              itemHeight: 18,
+              itemTextColor: '#999',
+              itemDirection: 'right-to-left',
+              itemOpacity: 1,
+              symbolSize: 18,
+              symbolShape: 'square',
+              effects: [
+                  {
+                      on: 'hover',
+                      style: {
+                          itemTextColor: '#000'
+                      }
+                  }
+              ]
+          }
+      ]}
   />
+    )
+
+
     return (
-  <ResponsiveMarimekko />
-)
+      <>
+        <MyResponsivemike/>
+      </>
+    )
   }
-  */
 
 
-return (
-  <div>
-    <Sidebar />
-    <main className="col-11 offset-1 col-lg-11 offset-lg-1 px-5">
-      <div className="row mt-5 pb-3 main-header">
-        <h1 className="col-lg-3 fs-2">Home - desafio 2.0</h1>
-        <input
-          className="col-lg-3 offset-lg-6"
-          type="search"
-          name="main-search"
-          id="main-search"
-          placeholder="Pesquise aqui..."
-        />
-      </div>
-
-      <div className="Options row align-items-center mt-lg-4">
-        <div className="LeftOptions col-lg-2 mt-sm-2">
-          <span className="me-2 fs-4">Projetos recentes</span>
+  return (
+    <div>
+      <Sidebar />
+      <main className="col-11 offset-1 col-lg-11 offset-lg-1 px-5">
+        <div className="row mt-5 pb-3 main-header">
+          <h1 className="col-lg-3 fs-2">Home - desafio 2.0</h1>
+          <input
+            className="col-lg-3 offset-lg-6"
+            type="search"
+            name="main-search"
+            id="main-search"
+            placeholder="Pesquise aqui..."
+          />
         </div>
-        <div className="RightOptions d-flex col-lg-2 offset-lg-8 mt-sm-2">
-          <AdicionarProjeto atualiza={Atualizar} />
-        </div>
-      </div>
 
-      <div
-        className="row d-flex justify-content-between flex-wrap"
-      >
-        <div className="CardlateralesquerdoHome col-lg-6 col-md-12 mt-2 ms-3">
-          <div className="row CardsContainer my-4 mt-4 d-flex">
-            <Cards Projetos={projetos} atualiza={Atualizar} home={"home"} />
+        <div className="Options row align-items-center mt-lg-4">
+          <div className="LeftOptions col-lg-2 mt-sm-2">
+            <span className="me-2 fs-4">Projetos recentes</span>
+          </div>
+          <div className="RightOptions d-flex col-lg-2 offset-lg-8 mt-sm-2">
+            <AdicionarProjeto atualiza={Atualizar} />
           </div>
         </div>
-        <div className="cardLateralHome d-flex justify-content-center col-lg-6 col-md-12 col-sm-12 mt-2 p-4 mt-4">
-          <div className="Resumo col-md-12 col-lg-12 justify-content-center ">
-            <div className="TotColaboradores d-flex align-items-center justify-content-center col-12">
-              <h6 >{QtdTotalEquipes}</h6>
-              <strong>
-                <p className="ms-4 ">Total de <br /> Equipes</p>
-              </strong>
-              <h6 className="ms-5" style={{ color: "var(--azul-claro)" }}>{QtdTotalPessoas}</h6>
-              <strong>
-                <p className="ms-4 ">Total de <br /> pessoas</p>
-              </strong>
+
+        <div
+          className="row d-flex justify-content-between flex-wrap"
+        >
+          <div className="CardlateralesquerdoHome col-lg-6 col-md-12 mt-2 ms-3">
+            <div className="row CardsContainer my-4 mt-4 d-flex">
+              <Cards Projetos={projetos} atualiza={Atualizar} home={"home"} />
             </div>
-            <div className="row col-12">
-              <div className="TotTarefas col-6 d-flex flex-column align-items-center justify-content-center">
-                <h6 className="col">{QtdProjetos}</h6>
+          </div>
+          <div className="cardLateralHome d-flex justify-content-center col-lg-6 col-md-12 col-sm-12 mt-2 p-4 mt-4">
+            <div className="Resumo col-md-12 col-lg-12 justify-content-center ">
+             <div className="TotColaboradores d-flex align-items-center justify-content-center col-12">
+                <h6 >{QtdTotalEquipes}</h6>
                 <strong>
-                  <p className="text-center col">Total de <br /> Projetos</p>
+                  <p className="ms-4 ">Total de <br /> Equipes</p>
+                </strong>
+                <h6 className="ms-5" style={{ color: "var(--azul-claro)" }}>{QtdTotalPessoas}</h6>
+                <strong>
+                  <p className="ms-4 ">Total de <br /> pessoas</p>
                 </strong>
               </div>
-              <div className="col-6 d-flex flex-column align-items-center justify-content-center">
-                <div className="TarefasAnd d-flex align-items-center justify-content-center">
-                  <h6 className="col-4 md-5" style={{ fontFamily: "'Roboto Mono', monospace" }}>{QtdAndamento}</h6>
+              <div className="row col-12">
+                <div className="TotTarefas col-6 d-flex flex-column align-items-center justify-content-center">
+                  <h6 className="col">{QtdProjetos}</h6>
                   <strong>
-                    <p className="ms-2">Projetos em Andamento</p>
+                    <p className="text-center col">Total de <br /> Projetos</p>
                   </strong>
                 </div>
-                <div className="TarefasConc d-flex align-items-center justify-content-center">
-                  <h6 className="col-4 md-5" style={{ fontFamily: "'Roboto Mono', monospace" }}>{QtdConcluidos}</h6>
-                  <strong>
-                    <p className=" ms-2">Projetos Concluídos</p>
-                  </strong>
+                <div className="col-6 d-flex flex-column align-items-center justify-content-center">
+                  <div className="TarefasAnd d-flex align-items-center justify-content-center">
+                    <h6 className="col-4 md-5" style={{ fontFamily: "'Roboto Mono', monospace" }}>{QtdAndamento}</h6>
+                    <strong>
+                      <p className="ms-2">Projetos em Andamento</p>
+                    </strong>
+                  </div>
+                  <div className="TarefasConc d-flex align-items-center justify-content-center">
+                    <h6 className="col-4 md-5" style={{ fontFamily: "'Roboto Mono', monospace" }}>{QtdConcluidos}</h6>
+                    <strong>
+                      <p className=" ms-2">Projetos Concluídos</p>
+                    </strong>
+                  </div>
                 </div>
               </div>
+               {/*<Grafico/>*/}
             </div>
-          </div>
 
+          </div>
         </div>
-      </div>
-      <div className="Teste row  h-lg-50 p-3 mt-2">
-        <CardBaixoHome Projetos={projetos} />
-      </div>
-    </main>
-  </div>
-);
+        <div className="Teste row  h-lg-50 p-3 mt-2">
+          <CardBaixoHome Projetos={projetos} />
+        </div>
+      </main>
+    </div>
+  );
 }
