0d27a1b52fab3ed2b1fae6754eae7d42ab393768
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-08-22 11:51:34 -0300

    Update disablePast.js

--- src/pages/Projetos/components/DatePicker/DisablePast/disablePast.js
+++ src/pages/Projetos/components/DatePicker/DisablePast/disablePast.js
@@ -3,9 +3,7 @@ import TextField from '@mui/material/TextField';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import FilterPopper from '../../FilterPopper';
-import { alpha, styled } from '@mui/material/styles';
-import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
 
 export default function CssDatePickerPas(props) {
   const [value, setValue] = React.useState(new Date());
