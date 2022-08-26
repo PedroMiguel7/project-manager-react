c77d9ee1002a19bbf89cecc31beb1e929bba6650
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-08-26 09:50:10 -0300

    testando no vercel

--- src/api.js
+++ src/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/',
-  mode: "no-cors"
+  baseURL: 'https://api-brisa-react.vercel.app'
 });
 
 const token = localStorage.getItem('token')
