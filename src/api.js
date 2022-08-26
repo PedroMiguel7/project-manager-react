e1b4d51581b4f15314392d28570e03d72d9b880f
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-08-26 08:43:48 -0300

    mais um teste no vercel

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://api-brisa-react.vercel.app/',
+  baseURL: 'https://golang-posgre-brisanet.herokuapp.com/',
   mode: "no-cors"
 });
 
