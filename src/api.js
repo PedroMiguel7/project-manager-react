9b9807cc41afb0a48b74fc32607b44dabb02d01f
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-08-26 10:31:58 -0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://golang-posgre-brisanet.herokuapp.com/'
+  baseURL: 'https://golang-posgre-brisanet.herokuapp.com'
 });
 
 const token = localStorage.getItem('token')
