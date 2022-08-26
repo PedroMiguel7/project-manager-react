e9696131913f71d5de963352a663e08ef2b587dc
dayannebugarim <81201377+dayannebugarim@users.noreply.github.com>
2022-08-26 09:54:15 -0300

    Ajustes no skeleton dos cards de projetos

--- src/components/Card/CardProjetos/Skeleton/index.js
+++ src/components/Card/CardProjetos/Skeleton/index.js
@@ -5,24 +5,22 @@ export default function CardSkeleton(){
     return (
         <>
             <Stack spacing={2} className="Card">
-            {/* For variant="text", adjust the height via font-size */}
-                <Skeleton variant="text" sx={{ fontSize: '2.2rem', borderRadius: "10px" }} />
+                <Skeleton variant="text" sx={{ fontSize: '2rem', borderRadius: "8px" }} />
                 <Stack height={120}>
-                    <Skeleton variant="text" sx={{ fontSize: '1rem', borderRadius: "10px" }} />
-                    <Skeleton variant="text" sx={{ fontSize: '1rem', borderRadius: "10px" }} />
-                    <Skeleton variant="text" sx={{ fontSize: '1rem', borderRadius: "10px" }} />
-                    <Skeleton variant="text" sx={{ fontSize: '1rem', borderRadius: "10px" }} />
+                    <Skeleton variant="text" sx={{ fontSize: '0.8rem', borderRadius: "8px" }} />
+                    <Skeleton variant="text" sx={{ fontSize: '0.8rem', borderRadius: "8px" }} />
+                    <Skeleton variant="text" sx={{ fontSize: '0.8rem', borderRadius: "8px", width: "70%" }} />
                 </Stack>
-                <Skeleton variant="text" width="35%" sx={{ fontSize: '1rem', borderRadius: "10px" }} />
+                <Skeleton variant="text" width="35%" sx={{ fontSize: '0.8rem', borderRadius: "8px", mb: 5 }} />
 
-                <Stack spacing={4} justifyContent="space-between" direction="row">
-                    <Stack width="100%">
-                        <Skeleton variant="text" sx={{ fontSize: '1rem', borderRadius: "10px" }} />
-                        <Skeleton variant="text" sx={{ fontSize: '1rem', borderRadius: "10px" }} width="60%" />
+                <Stack spacing={5} justifyContent="space-between" direction="row">
+                    <Stack width="80%">
+                        <Skeleton variant="text" sx={{ fontSize: '1rem', borderRadius: "8px" }} />
+                        <Skeleton variant="text" sx={{ fontSize: '0.8rem', borderRadius: "8px" }} width="60%" />
                     </Stack>
-                    <Stack width="100%">
-                        <Skeleton variant="text" sx={{ fontSize: '1rem', borderRadius: "10px" }} />
-                        <Skeleton variant="text" sx={{ fontSize: '1rem', borderRadius: "10px" }} width="60%" />
+                    <Stack width="80%">
+                        <Skeleton variant="text" sx={{ fontSize: '1rem', borderRadius: "8px" }} />
+                        <Skeleton variant="text" sx={{ fontSize: '0.8rem', borderRadius: "8px" }} width="60%" />
                     </Stack>
                 </Stack>
             </Stack>
