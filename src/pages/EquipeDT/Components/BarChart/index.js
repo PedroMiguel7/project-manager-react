d554746242a8855262b4a17eda0341152c87d750
Dayanne <81201377+dayannebugarim@users.noreply.github.com>
2022-08-30 20:44:29 -0300

    Condicional p/ renderizar o gráfico de barras

--- src/pages/EquipeDT/Components/BarChart/index.js
+++ src/pages/EquipeDT/Components/BarChart/index.js
@@ -37,107 +37,112 @@ export default function BarChart(props) {
 
     return (
         <>
-            <div style={{ width: "100%", height: "220px" }}>
-                <ResponsiveBar
-                    data={data}
-                    keys={[
-                        'qtd',
-                    ]}
-                    indexBy="Membro"
-                    margin={{ top: 0, right: 20, bottom: 18, left: 85 }}
-                    padding={0.2}
-                    layout="horizontal"
-                    valueScale={{ type: 'linear' }}
-                    indexScale={{ type: 'band', round: true }}
-                    colors={{ scheme: 'nivo' }}
-                    colorBy="indexValue"
-                    theme={{
-                        textColor: "#C2C3C6",
-                        fontSize: 13,
-                        grid: {
-                            line: {
-                                stroke: "#87888C",
-                                strokeWidth: 0.2
-                            }
-                        },
-                        tooltip: {
-                            container: {
-                                background: "#87888c",
-                                color: "#fff",
-                                fontSize: 14
+            {
+                Object.keys(counts).length < 3 ? 
+                <h1>Tem nada n</h1> 
+                : 
+                <div style={{ width: "100%", height: "220px" }}>
+                    <ResponsiveBar
+                        data={data}
+                        keys={[
+                            'qtd',
+                        ]}
+                        indexBy="Membro"
+                        margin={{ top: 0, right: 20, bottom: 18, left: 85 }}
+                        padding={0.2}
+                        layout="horizontal"
+                        valueScale={{ type: 'linear' }}
+                        indexScale={{ type: 'band', round: true }}
+                        colors={{ scheme: 'nivo' }}
+                        colorBy="indexValue"
+                        theme={{
+                            textColor: "#C2C3C6",
+                            fontSize: 13,
+                            grid: {
+                                line: {
+                                    stroke: "#87888C",
+                                    strokeWidth: 0.2
+                                }
                             },
-                        }
-                    }}
-                    defs={[
-                        {
-                            id: 'dots',
-                            type: 'patternDots',
-                            background: 'inherit',
-                            color: '#38bcb2',
-                            size: 4,
-                            padding: 1,
-                            stagger: true
-                        },
-                        {
-                            id: 'lines',
-                            type: 'patternLines',
-                            background: 'inherit',
-                            color: '#eed312',
-                            rotation: -45,
-                            lineWidth: 6,
-                            spacing: 10
-                        }
-                    ]}
-                    borderRadius={6}
-                    borderColor={{
-                        from: 'color',
-                        modifiers: [
-                            [
-                                'darker',
-                                1.6
+                            tooltip: {
+                                container: {
+                                    background: "#87888c",
+                                    color: "#fff",
+                                    fontSize: 14
+                                },
+                            }
+                        }}
+                        defs={[
+                            {
+                                id: 'dots',
+                                type: 'patternDots',
+                                background: 'inherit',
+                                color: '#38bcb2',
+                                size: 4,
+                                padding: 1,
+                                stagger: true
+                            },
+                            {
+                                id: 'lines',
+                                type: 'patternLines',
+                                background: 'inherit',
+                                color: '#eed312',
+                                rotation: -45,
+                                lineWidth: 6,
+                                spacing: 10
+                            }
+                        ]}
+                        borderRadius={6}
+                        borderColor={{
+                            from: 'color',
+                            modifiers: [
+                                [
+                                    'darker',
+                                    1.6
+                                ]
                             ]
-                        ]
-                    }}
-                    axisTop={null}
-                    axisRight={null}
-                    axisBottom={{
-                        tickSize: 0,
-                        tickPadding: 6,
-                        tickRotation: 0,
-                    }}
-                    axisLeft={{
-                        format: (v) => {
-                            return v?.length > 9 ? (
-                                <tspan>
-                                    {v.substring(0, 9) + "..."}
-                                    <title>{v}</title>
-                                </tspan>
-                            ) : (
-                                v
-                            );
-                        },
-                        tickSize: 0,
-                        tickPadding: 9,
-                        tickRotation: 0,
-                    }}
-                    enableGridX={true}
-                    enableGridY={false}
-                    labelTextColor={{
-                        from: 'color',
-                        modifiers: [
-                            [
-                                'darker',
-                                1.6
+                        }}
+                        axisTop={null}
+                        axisRight={null}
+                        axisBottom={{
+                            tickSize: 0,
+                            tickPadding: 6,
+                            tickRotation: 0,
+                        }}
+                        axisLeft={{
+                            format: (v) => {
+                                return v?.length > 9 ? (
+                                    <tspan>
+                                        {v.substring(0, 9) + "..."}
+                                        <title>{v}</title>
+                                    </tspan>
+                                ) : (
+                                    v
+                                );
+                            },
+                            tickSize: 0,
+                            tickPadding: 9,
+                            tickRotation: 0,
+                        }}
+                        enableGridX={true}
+                        enableGridY={false}
+                        labelTextColor={{
+                            from: 'color',
+                            modifiers: [
+                                [
+                                    'darker',
+                                    1.6
+                                ]
                             ]
-                        ]
-                    }}
-                    legends={[]}
-                    tooltip={function () { }}
-                    role="application"
-                    ariaLabel="Nivo bar chart demo"
-                    barAriaLabel={function (e) { return e.id + ": " + e.formattedValue + " in country: " + e.indexValue }}
-                />
-            </div>
+                        }}
+                        legends={[]}
+                        tooltip={function () { }}
+                        role="application"
+                        ariaLabel="Nivo bar chart demo"
+                        barAriaLabel={function (e) { return e.id + ": " + e.formattedValue + " in country: " + e.indexValue }}
+                    />
+                </div>
+            }
         </>
     )
 }
