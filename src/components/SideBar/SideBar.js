1416db0c5dd6baa8c87eb7f4fe9b6f54ca8bac08
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-08-30 11:57:18 -0300

    botao de mudar tema apenas inserido
    falta função e estilização

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,18 +1,8 @@
-import { useLocation } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
 import TemporaryDrawer from "./Drawer";
 import Fixed from "./Fixed";
 
 function SideBar() {
-    const navigate = useNavigate();
-    const signout = (e) =>{
-        e.preventDefault()
-        localStorage.removeItem('token')
-    }
-    
     let path = window.location.pathname;
-    let pathId = path.substring(path.lastIndexOf('/') + 1);
-    const location = useLocation();
     if(path === '/' | localStorage.getItem("token") === null){
         return;
     }
