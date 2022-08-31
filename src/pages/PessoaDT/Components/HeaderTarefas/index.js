40a076ed09dce7528f1c0ec21355c286e321605d
Dayanne <81201377+dayannebugarim@users.noreply.github.com>
2022-08-30 21:14:44 -0300

    Criação dos outros filtros de tarefas

--- src/pages/PessoaDT/Components/HeaderTarefas/index.js
+++ src/pages/PessoaDT/Components/HeaderTarefas/index.js
@@ -1,12 +1,14 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import TarefasAndamento from '../TabelaTarefas/TarefasAndamento';
-import TarefasConcluidas from '../TabelaTarefas/TarefasConcluidas';
-import ContadorTarefas from '../ContadorTarefas';
+import * as React from "react";
+import PropTypes from "prop-types";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import TarefasFazer from "../TabelaTarefas/TarefasFazer";
+import TarefasAndamento from "../TabelaTarefas/TarefasAndamento";
+import TarefasTeste from "../TabelaTarefas/TarefasTeste";
+import TarefasConcluidas from "../TabelaTarefas/TarefasConcluidas";
+import ContadorTarefas from "../ContadorTarefas";
 import { HeaderContainer, Header, Title, TableContainer, Table } from "./style";
 
 function TabPanel(props) {
@@ -20,11 +22,7 @@ function TabPanel(props) {
       aria-labelledby={`simple-tab-${index}`}
       {...other}
     >
-      {value === index && (
-        <Box >
-          {children}
-        </Box>
-      )}
+      {value === index && <Box>{children}</Box>}
     </div>
   );
 }
@@ -38,7 +36,7 @@ TabPanel.propTypes = {
 function a11yProps(index) {
   return {
     id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
   };
 }
 
@@ -48,34 +46,34 @@ const StyledTabs = styled((props) => (
     TabIndicatorProps={{ children: <span className="MuiTabs-indicatorSpan" /> }}
   />
 ))({
-  '& .MuiTabs-indicator': {
-    display: 'flex',
-    justifyContent: 'center',
-    backgroundColor: 'transparent',
+  "& .MuiTabs-indicator": {
+    display: "flex",
+    justifyContent: "center",
+    backgroundColor: "transparent",
   },
-  '& .MuiTabs-indicatorSpan': {
-    maxWidth: '70%',
-    width: '100%',
-    backgroundColor: '#F46E27',
+  "& .MuiTabs-indicatorSpan": {
+    maxWidth: "70%",
+    width: "100%",
+    backgroundColor: "#F46E27",
   },
 });
 
 const StyledTab = styled((props) => <Tab disableRipple {...props} />)(
   ({ theme }) => ({
-    fontFamily: 'Inter, sans-serif',
-    textTransform: 'none',
+    fontFamily: "Inter, sans-serif",
+    textTransform: "none",
     fontWeight: 400,
     fontSize: 14,
     marginRight: theme.spacing(1),
-    color: '#87888C',
-    '&.Mui-selected': {
-      color: '#C2C3C6',
+    color: "#87888C",
+    "&.Mui-selected": {
+      color: "#C2C3C6",
       fontWeight: 500,
     },
-    '&.Mui-focusVisible': {
-      backgroundColor: 'rgba(100, 95, 228, 0.32)',
+    "&.Mui-focusVisible": {
+      backgroundColor: "rgba(100, 95, 228, 0.32)",
     },
-  }),
+  })
 );
 
 export default function BasicTabs() {
@@ -90,34 +88,49 @@ export default function BasicTabs() {
       <HeaderContainer>
         <Header>
           <Title>Tarefas</Title>
-          <StyledTabs className="mx-4" value={value} onChange={handleChange} aria-label="basic tabs" centered>
-            <StyledTab className="StyledTab" label="Em Andamento" {...a11yProps(0)} />
-            <StyledTab className="StyledTab" label="Concluídas" {...a11yProps(1)} />
+          <StyledTabs
+            className="mx-4"
+            value={value}
+            onChange={handleChange}
+            aria-label="basic tabs"
+            centered
+          >
+            <StyledTab
+              className="StyledTab"
+              label="A Fazer"
+              {...a11yProps(0)}
+            />
+            <StyledTab
+              className="StyledTab"
+              label="Em Andamento"
+              {...a11yProps(1)}
+            />
+            <StyledTab
+              className="StyledTab"
+              label="Em Teste"
+              {...a11yProps(2)}
+            />
+            <StyledTab
+              className="StyledTab"
+              label="Concluídas"
+              {...a11yProps(3)}
+            />
           </StyledTabs>
         </Header>
         <ContadorTarefas />
       </HeaderContainer>
 
       <TabPanel value={value} index={0}>
-        <TarefasAndamento index={0} />
+        <TarefasFazer index={0} />
       </TabPanel>
       <TabPanel value={value} index={1}>
-        <TarefasConcluidas index={1} />
+        <TarefasAndamento index={1} />
       </TabPanel>
       <TabPanel value={value} index={2}>
-        
-        <TableContainer className='table-responsive'>
-          <Table className="align-middle text-center ">
-            <tbody>
-              <tr>
-                <th scope="col"></th>
-                <th scope="col">Nome</th>
-                <th scope="col">Início</th>
-                <th scope="col"></th>
-              </tr>
-            </tbody>
-          </Table>
-        </TableContainer>
+        <TarefasTeste index={2} />
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+        <TarefasConcluidas index={3} />
       </TabPanel>
     </>
   );
---
+++ src/pages/PessoaDT/Components/TabelaTarefas/TarefasFazer/index.js
@@ -0,0 +1,324 @@
+import * as React from 'react';
+import { Component } from "react";
+import api from '../../../../../api';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import TarefasMenu from '../../TarefasMenu';
+import IconButton from '@mui/material/IconButton';
+import TaskAltRoundedIcon from '@mui/icons-material/TaskAltRounded';
+import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogTitle from '@mui/material/DialogTitle';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+import PrioridadeAlta from '../../../../../assets/icons/prioridade-alta.svg';
+import PrioridadeMedia from '../../../../../assets/icons/prioridade-media.svg';
+import PrioridadeBaixa from '../../../../../assets/icons/prioridade-baixa.svg';
+import Tooltip from '@mui/material/Tooltip';
+import TasksNotFound from "../../../../../assets/empty-states/tasks-not-found.svg";
+import { NoBorder, Head, TableContainer, Table, Row, PriorityIcons } from './style';
+
+
+class TarefasFazer extends Component {
+  state = {
+    tarefas: [],
+    tarefasId: 0,
+    openAlert: false,
+    openSnackbar: false,
+    changeIcon: false,
+  }
+  async componentDidMount() {
+      const pessoaPath = window.location.pathname;
+      
+      const response = await api.get(pessoaPath+'/tasks');
+
+      this.setState({ tarefas: response.data });
+  }
+
+  Header = (props) => {
+    const qtdTarefas = props.tarefas;
+    
+    if (qtdTarefas !== null) {
+      var qtdFazer = qtdTarefas?.filter((tarefas) => tarefas.status === "A Fazer");  
+    }
+
+    if( qtdTarefas === null || qtdFazer.length === 0){
+      return (
+        <>
+        </>
+      )
+    } else {
+      return (
+        <>
+          <Head>
+            <th scope="col"></th>
+            <th scope="col">Nome</th>
+            <th scope="col">Prioridade</th>
+            {/* <th scope="col">Tempo Restante</th>
+             */}
+            <th scope="col">Data de Criação</th>
+            <th scope="col"></th>
+          </Head>
+        </>
+      )
+    }
+  }
+
+  ImprimeTarefas = (props) => {
+    const qtdTarefas = props.tarefas;
+
+    if (qtdTarefas !== null) {
+      var qtdFazer = qtdTarefas?.filter((tarefas) => tarefas.status === "A Fazer");  
+    }
+
+    const [openAlert, setOpenAlert] = React.useState(false);
+
+    const handleClose = (event, reason) => {
+      if (reason === 'clickaway') {
+        return;
+      }
+      setOpenSnackbar(false);
+    };
+    
+    const handleCheck = (id) => {
+      console.log(id);
+      this.setState({tarefasId: id})
+      this.setState({openAlert: true})
+    }
+
+    const handleCloseAlert = () => {
+      setOpenAlert(false);
+    }
+
+    const [openSnackbar, setOpenSnackbar] = React.useState(false);
+
+    const handleClickSim = (id) => {
+      EditaTask(id);
+      setOpenSnackbar(true);
+      setOpenAlert(false);
+      this.setState({changeIcon: true});
+    };
+
+    const icon = (this.state.changeIcon === true) ? <TaskAltRoundedIcon sx={{color: "#F46E27"}} /> : <RadioButtonUncheckedIcon sx={{color: "#C2C3C6"}}/>;
+
+    function EditaTask(id) {
+      api.put('/tasks/' + id + '/status', {
+        status: "Concluido",
+      })
+      .then(
+        props.atualiza()
+      )
+    }
+
+    if( qtdTarefas === null || qtdFazer.length === 0){
+        return(
+          <>
+            <NoBorder>
+              <img src={TasksNotFound} />
+              <h5 style={{color: "#454756", textAlign: "center"}}>
+                Sem tarefas a fazer.
+              </h5>
+            </NoBorder>
+          </>
+          
+        );
+    } else {
+        function Prioridade(prioridade) {
+          if (prioridade === 0) {
+            return (
+              <Tooltip title='Baixa' arrow>
+                <PriorityIcons src={PrioridadeBaixa}/>
+              </Tooltip>
+            )
+          } else if (prioridade == 1) {
+            return (
+              <Tooltip title='Média' arrow>
+                <PriorityIcons src={PrioridadeMedia}/>
+              </Tooltip>
+            )
+          } else if (prioridade === 2) {
+            return (
+              <Tooltip title='Alta' arrow>
+                <PriorityIcons src={PrioridadeAlta}/>
+              </Tooltip>
+            )
+          }
+        }
+
+        function TempoRestante(prazo) {
+          const dataHojeFormatada = new Date();
+
+          const dataPrazoFormatada = new Date(prazo);
+          
+          if ((dataPrazoFormatada - dataHojeFormatada) <= 86400000 && (dataPrazoFormatada - dataHojeFormatada) > 0) {
+            return '1 dia';
+          } else if (dataPrazoFormatada < dataHojeFormatada) {
+            return 'Atraso'
+          } else {
+            const tempo = (dataPrazoFormatada - dataHojeFormatada) / 86400000;
+            return `${tempo.toFixed()} dias`;
+          }
+        }
+
+        function Inicio(inicio) {
+          const dataInicio = new Date(inicio);
+          dataInicio.setDate(dataInicio.getDate() + 1);
+          const dataInicioFormatada = dataInicio.toLocaleDateString("pt-BR");
+          return dataInicioFormatada;
+        }
+
+        var getId;
+        return (
+          props.tarefas?.map(t => 
+            {if (t.status === "A Fazer")
+            return (
+              <>
+                <Row id={t.id_task}>
+                  <td>
+                    <IconButton onClick={() => {handleCheck(t.id_task);}}>
+                      {icon}
+                    </IconButton>
+                  </td>
+                  <td>
+                    {t.descricao_task}
+                  </td>
+                  <td>
+                    {Prioridade(t.prioridade)}
+                  </td>
+                  {/* <td>
+                    <AccessTimeIcon sx={{fontSize: '1.1rem', marginRight: '0.2rem'}} /> 
+                    {TempoRestante(t.prazo_entrega)}
+                  </td>
+                   */}
+                  <td>
+                    {Inicio(t.data_criacao)}
+                  </td>
+                  <td>
+                    <TarefasMenu equipe_id={t.id_equipe} id_task={t.id_task} />
+                  </td>
+                </Row>
+              </>
+            )
+          }
+        ));
+    }
+  }
+
+  Alerta = () => {
+    const handleClose = (event, reason) => {
+      if (reason === 'clickaway') {
+        return;
+      }
+      setOpenSnackbar(false);
+    };
+
+    const handleCheck = (id) => {
+
+      this.setState({tarefasId: id})
+      this.setState({openAlert: true});
+    }
+
+    const handleCloseAlert = () => {
+      this.setState({openAlert: false});
+    }
+
+    const [openSnackbar, setOpenSnackbar] = React.useState(false);
+
+    const handleClickSim = (id) => {
+      EditaTask(id);
+      setOpenSnackbar(true);
+      this.setState({openAlert: false});
+      console.log(this.state.tarefasId);
+    };
+
+    function EditaTask(id) {
+      api.put('/tasks/' + id + '/status', {
+        status: "Em Andamento",
+        data_criacao: new Date()
+      })
+      .then(
+        //props.atualiza()
+      )
+    }
+
+    return(
+      <>
+        <Dialog
+          open={this.state.openAlert}
+          //key={t.id_task}
+          onClose={handleCloseAlert}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+          PaperProps={{
+              style: {
+                backgroundColor: '#494A58',
+                color: '#fff'
+              },
+            }}
+          >
+              <DialogTitle id="alert-dialog-title">
+              {"Deseja inciar esta tarefa?"}
+              </DialogTitle>
+              
+              <DialogActions>
+              <Button onClick={handleCloseAlert}
+              sx={{
+                  color: "#C2C3C6",
+                  opacity: 0.7
+              }}>Não</Button>
+              <Button autoFocus onClick={() => {handleClickSim(this.state.tarefasId);}} variant="contained"
+              sx={{
+                  color: "#FFF",
+                  backgroundColor: "#F57D3D",
+                  '&:hover': {
+                      backgroundColor: "#F46E27",
+                  }
+              }}>
+                  Sim
+              </Button>
+              </DialogActions>
+          </Dialog>
+          <Snackbar open={openSnackbar} autoHideDuration={2500} onClose={handleClose} anchorOrigin={{vertical: 'top', horizontal: 'center',}}>
+            <MuiAlert onClose={handleClose} severity="success" elevation={6} variant="filled" sx={{ minWidth: '20vw' }}>
+              Tarefa concluída!
+            </MuiAlert>
+        </Snackbar>
+      </>
+    )
+  }
+
+  render() {
+    const { tarefas } = this.state;
+
+    var TableHeader = document.getElementsByClassName('TabelaTarefasHead');
+    
+    const scrollHandler = (event) => {
+      //var TableHeader = document.getElementsByClassName('TabelaTarefasHead');
+      //console.log(TableHeader);
+
+      let ScrollValue = event.currentTarget.scrollTop;
+      //console.log(ScrollValue);
+
+      if (ScrollValue < 4) {
+        //console.log(document.getElementsByClassName(".TabelaTarefasHead").remove("AddShadow"))
+      }
+    }
+
+    return (
+      <>
+        <TableContainer className='table-responsive' /*onScroll={scrollHandler}*/>
+          <Table id='table' className="table align-middle text-center">
+            <tbody>
+              <this.Header tarefas={tarefas} />
+              <this.ImprimeTarefas tarefas={tarefas} />
+              <this.Alerta />
+            </tbody>
+          </Table>
+        </TableContainer>
+      </>
+    )
+  }
+}
+
+export default TarefasFazer;
