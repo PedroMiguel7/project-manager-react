4946da9c2b1f9aa5e5257e3ccf51ba86168227aa
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-09-08 09:01:32 -0300

    refactor: trocando 'class' por 'className'
    por causa do reactjs

--- src/components/Card/CardProjetos/BarraDeProgresso.js
+++ src/components/Card/CardProjetos/BarraDeProgresso.js
@@ -32,8 +32,8 @@ const [tasks, setTasks] = useState([]);
 
     return (
         <>
-            <div class="progress mt-2" style={{ backgroundColor: "gray" }}>
-                <div class="progress-bar" role="progressbar" style={{ backgroundColor: "var(--corBotaoInverso)", width:`${BarrinhaProgresso}%` }} aria-valuenow="25"  aria-valuemin="0" aria-valuemax="100">{Math.round(BarrinhaProgresso)}%</div>
+            <div className="progress mt-2" style={{ backgroundColor: "gray" }}>
+                <div className="progress-bar" role="progressbar" style={{ backgroundColor: "var(--corBotaoInverso)", width:`${BarrinhaProgresso}%` }} aria-valuenow="25"  aria-valuemin="0" aria-valuemax="100">{Math.round(BarrinhaProgresso)}%</div>
             </div>
         </>
     )
