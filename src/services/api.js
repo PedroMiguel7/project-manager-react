aed8830710f154a97538da1726b850a191f5b19c
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-09-09 11:00:36 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  //baseURL: 'http://localhost:3000/'
-  baseURL: 'https://golang-posgre-brisanet.herokuapp.com/',
+  baseURL: 'http://localhost:3000/',
+  //baseURL: 'https://golang-posgre-brisanet.herokuapp.com/',
+  mode: 'no-cors'
 });
 
 const token = localStorage.getItem('token')
