e52cfccf595cc316b63f7a78ef066cbbf520bd4d
PedroMiguel7 <93842972+PedroMiguel7@users.noreply.github.com>
2022-09-10 11:36:25 -0300

    Update login-copy-1.cy.js

--- cypress/e2e/MAIN/login-copy-1.cy.js
+++ cypress/e2e/MAIN/login-copy-1.cy.js
@@ -3,13 +3,6 @@ describe('Test in my login page', () => {
   beforeEach(() => {
     cy.visit('/')
   })
-
-  // cy.findAllByPlaceholderText(/email/i).type('root@root.com')
-  // cy.findAllByPlaceholderText(/senha/i).type('salmo34')
-
-  // cy.findByRole('button', {name: /LOGIN/i}.click()
-
-  // cy.url().should('eq', `${Cypress.config().baseUrl}/home`))
   
   it('Sign ', () => {
     cy.get('#email').type('root@root.com');
