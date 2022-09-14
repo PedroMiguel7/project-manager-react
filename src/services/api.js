10050a889a748da3af93b656b38e3ca5be28dc86
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-09-14 10:56:42 -0300

    test: vercel funcionar

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,6 @@ const api = axios.create({
   //baseURL: 'http://localhost:3000/',
   baseURL: 'https://golang-posgre-brisanet.herokuapp.com/',
   //baseURL: 'https://api-brisa-react.vercel.app/',
-  mode: 'no-cors'
 });
 
 const token = localStorage.getItem('token')
