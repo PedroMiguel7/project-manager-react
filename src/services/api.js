4bf2212b7e93441102dc69e9ac5f85e3764425f1
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-09-14 08:29:50 -0300

    test: pro vercel, configurando proxys do npm e do yarn

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
   //baseURL: 'http://localhost:3000/',
-  baseURL: 'https://golang-posgre-brisanet.herokuapp.com/',
-  //baseURL: 'https://api-brisa-react.vercel.app/',
+  //baseURL: 'https://golang-posgre-brisanet.herokuapp.com/',
+  baseURL: 'https://api-brisa-react.vercel.app/',
   mode: 'no-cors'
 });
 
