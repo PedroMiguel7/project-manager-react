fc5d75b593d4932185c8c3c816d7637ec5864cd4
Pedro Miguel <93842972+PedroMiguel7@users.noreply.github.com>
2022-09-14 09:47:24 -0300

    teste: pro vercel

--- src/services/api.js
+++ src/services/api.js
@@ -2,15 +2,16 @@ import axios from 'axios';
 
 const api = axios.create({
   //baseURL: 'http://localhost:3000/',
-  //baseURL: 'https://golang-posgre-brisanet.herokuapp.com/',
-  baseURL: 'https://api-brisa-react.vercel.app/',
+  baseURL: 'https://golang-posgre-brisanet.herokuapp.com/',
+  //baseURL: 'https://api-brisa-react.vercel.app/',
   mode: 'no-cors'
 });
 
 const token = localStorage.getItem('token')
 
 if (token) {
-  api.defaults.headers.common['Authorization'] = `Bearer ${JSON.parse(token)}`
+  Object.assign(api.defaults.headers.common, { Authorization: `Bearer ${JSON.parse(token)}` });
 }
+console.log(api.defaults.headers.common['Authorization'])
 
 export default api;
